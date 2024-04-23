24cb00f472c4314bf1fb1fb9d42432dc337fd897
Maya <Mayastridevana0205@gmail.com>
2024-04-23 12:48:41 +0700

    Initial Commit

---
+++ VoaNews/VoaNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoaNewsApp.swift
+//  VoaNews
+//
+//  Created by User on 23/04/24.
+//
+
+import SwiftUI
+
+@main
+struct VoaNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
